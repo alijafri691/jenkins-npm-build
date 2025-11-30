6b4f5419a7c1c5e8126681a6a967f86439063613
Ali Jafri <112391820+alijafri691@users.noreply.github.com>
2025-11-30 16:50:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const http = require("http");
 
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer((req, res) => {
   res.end("Hello from Node app running through Jenkins!");
 });
 
-server.listen(8080, () => {
-  console.log("Server running on port 8080");
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
